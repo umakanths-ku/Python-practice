099e170e29e4cdc993004116d27f001385d7e677
umakanths-ku <umakanthsmp@gmail.com>
2026-05-17 22:00:04 +0530

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,17 +1,22 @@
+
+
+#this is a  simple menu problem using dictionary 
+
+
 menu= {"pizza": 3.9, 
        "buregr": 4.9,
        "pretzel": 3.9, 
        "dosa": 2.9, 
        "idli": 1.9} 
-cart=[]
-total=0 
+cart=[]#for entering the food to be purchased
+total=0 #to find the total cost of the food
 print(f"{"MENU":^16}")
 for key , value in menu.items(): 
     print(f"{key:10}: ${value:.2f}") 
 
 
 while True: #infinite loop  
-    food = input("Enter the food item(q to quit:")
+    food = input("Enter the food item(q to quit):")
     if food == "q": 
         break 
     elif menu.get(food) is not None: 

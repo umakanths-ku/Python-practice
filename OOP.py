eceae01b2ee00182e722a14011631910b817cf2a
umakanths-ku <umakanthsmp@gmail.com>
2026-06-06 10:49:13 +0530

    Create OOP.py
    Object orienteded programming..

---
+++ OOP.py
@@ -0,0 +1,12 @@
+from car import Car
+
+car1 = Car("Mustang",2024,"red",False) 
+car2=Car("Corvette",2025,"blue",True)
+car3 = Car("Alto",2024,"black",True)
+
+car1.describe()
+car2.describe()
+car3.describe()
+car1.drive()
+car2.drive()
+car3.stop()

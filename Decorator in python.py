7f2511d81e1c8b179b9f15358e1c259fa80a5417
umakanths-ku <umakanthsmp@gmail.com>
2026-06-09 11:08:16 +0530

    Create Decorator in python.py
    Decorator in pyhton

---
+++ Decorator in python.py
@@ -0,0 +1,21 @@
+# Decorator they extend the fuction of behaviour of a base function..
+
+def add_sprinkles(func):
+    def wrapper(*args,**kwargs):
+        print("*You add sprinkles🍒*")
+        func(*args,**kwargs)
+    return wrapper    
+
+def add_fudge(func):
+    def wrapper(*args,**kwargs):
+        print("You add fudge🍫")
+        func(*args,**kwargs)
+    return wrapper    
+
+
+@add_sprinkles
+@add_fudge
+def get_ice_cream(flavour):
+    print(f" Here is your {flavour} ice cream🍧")
+
+get_ice_cream("vanilla")

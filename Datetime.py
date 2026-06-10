54cfe424bdb69ef3f3945c29d918ae9e1ef9ec07
umakanths-ku <umakanthsmp@gmail.com>
2026-06-10 11:09:22 +0530

    Create Datetime.py
    Datetime in python

---
+++ Datetime.py
@@ -0,0 +1,18 @@
+import datetime
+
+date = datetime.date(2025,1,2)
+today = datetime.date.today() 
+
+time = datetime.time(12,30,0)
+now = datetime.datetime.now()
+
+now = now.strftime("%H:%M:%S %d-%m-%y")
+
+target_datetime = datetime.datetime(2000 ,1 ,2, 12, 30, 1)
+current_datetime = datetime.datetime.now() 
+
+if target_datetime < current_datetime:
+   print("Target date has passed")
+
+else:
+   print("Target date has NOT passed")   

8fca7b83b16e49e5e3a5334b0c50cf1dd3a49289
umakanths-ku <umakanthsmp@gmail.com>
2026-06-10 10:17:35 +0530

    Create Interaction with file in python2.py
    using json method.

---
+++ Interaction with file in python2.py
@@ -0,0 +1,17 @@
+import json
+
+employee = { 
+        "name" : "Spongebob",
+        "age"  : 30,
+        "job": "cook"
+
+}
+
+file_path = "C:/Users/HP/Desktop/test.json"
+try:
+    with open(file_path,"w") as file:
+        json.dump(employee,file, indent = 4)
+        print(f"json file {file_path} was created") 
+
+except FileExistsError:
+    print("That file already exists")        

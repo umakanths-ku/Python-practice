f65042093ba17a76a290d065b66c3492d62eb75f
umakanths-ku <umakanthsmp@gmail.com>
2026-06-11 23:40:06 +0530

    Update Alarm clock.py
    Alarm clock in python

--- Alarm clock.py
+++ Alarm clock.py
@@ -1,28 +1,28 @@
+import pygame
 import time 
 import datetime
-import winsound
 
-
-def set_alarm(alarm_time):
-    print(f"Alarm set for {alarm_time}")
+def alarm_time(set_alram):
+    
     is_running = True 
 
-    while is_running:
+    while is_running: 
         current_time = datetime.datetime.now().strftime("%H:%M:%S")
         print(current_time)
-
-        if current_time == alarm_time:
-            print("WAKE UP!!🗿")
-            winsound.Beep(1000,500)
-            time.sleep(1)
-
-            is_running = False
-
-        time.sleep(1)
-
         
+        if current_time == set_alram: 
+            print("WAKE UP!!😒")
+            is_running = False 
+            pygame.mixer.init()
+            pygame.mixer.music.load("background_track.mp3")
+            pygame.mixer.music.set_volume(0.7)
+            pygame.mixer.music.play(0)
+            while pygame.mixer.music.get_busy():
+                time.sleep(1)
 
+
+        time.sleep(1)      
 
 if __name__ == "__main__":
-    alram_time = input("Enter the alram time (HH:MM:SS): ")
-    set_alarm(alram_time)     
+    set_alram = input("Enter the alarm time: ")
+    alarm_time(set_alram)
